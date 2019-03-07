7fe98dcefd32c0f5ccaf13615d2e690e81484ba0
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-03-06 17:45:50 -0800

    Remove unnecessary exec < /dev/tty

--- pre_commit_hooks/cpplint.sh
+++ pre_commit_hooks/cpplint.sh
@@ -7,12 +7,10 @@ set -o nounset
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
 
-exec < /dev/tty
-
 echo 'Begin cpplint hook'
 
 if ! command -v cpplint &>/dev/null; then
-  >&2 echo 'cpplint command not found. See https://github.com/cpplint/cpplint for installation instructions.' 
+  >&2 echo 'cpplint command not found. See https://github.com/cpplint/cpplint for installation instructions.'
   exit 1
 fi
 
